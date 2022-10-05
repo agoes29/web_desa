af696ff5c012225fec334d1ba2ddebe3fbbee0d9
WikaAdnyana <adnyanawika676@gmail.com>
2022-10-05 11:17:42 +0800

    Test AdminLte

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/admin', function () {
+    return view('adminlte');
+});
