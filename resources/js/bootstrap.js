c224512f1e5085b5d63b2d2a8b4a9cf55182f58f
WikaAdnyana <adnyanawika676@gmail.com>
2022-10-05 14:00:42 +0800

    AdminLTE

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,10 @@
 import _ from 'lodash';
 window._ = _;
 
+import 'bootstrap';
+
+require('admin-lte');
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -32,4 +36,3 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
 //     enabledTransports: ['ws', 'wss'],
 // });
-require('admin-lte');
