044cb39acf5124543eab8a2f7fc8c7f4bc57c7ab
satriyadi <satriyadi3001@gmail.com>
2022-10-09 21:24:29 +0800

    pemisahan layout client

---
+++ resources/views/client/page/home.blade.php
@@ -0,0 +1,101 @@
+@extends('client.main')
+
+@section('home')
+    <main id="main">
+        <section id="about" class="about">
+            <div class="about-wrap container-fluid py-5" id="about">
+                <div class="container">
+                    <div class="row">
+                        <div class=" col-lg-5 col-md-6 order-1 order-md-2">
+                            <div class=" card gambar-about">
+                                <iframe
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15784.072586875232!2d115.13022005800802!3d-8.497615863724253!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd23b2bf99c0b95%3A0xc30e643bffae455e!2sBuahan%2C%20Kec.%20Tabanan%2C%20Kabupaten%20Tabanan%2C%20Bali!5e0!3m2!1sid!2sid!4v1664631528155!5m2!1sid!2sid"
+                                    width="500" height="350" style="border: 0" allowfullscreen="" loading="lazy"
+                                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            </div>
+                            <!-- <img src="assets/images/photo.jpg" alt="" /> -->
+                        </div>
+                        <div class="text-about col-lg-7 col-md-6 order-2 order-md-1">
+                            <div class="text-about">
+                                <h1 class="fw-bold">Desa Buahan</h1>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+                                    aspernatur ab, ullam totam fuga repellendus veniam possimus
+                                    voluptates rem fugiat dolorum optio ratione est repellat cum
+                                    inventore explicabo illo at.
+                                </p>
+                                <p>
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                                    Asperiores nemo laboriosam ab quisquam quas, impedit quaerat
+                                    quod nesciunt. Obcaecati possimus aliquam omnis iure,
+                                    perferendis natus voluptate commodi nisi itaque similique.
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Tempore sint earum atque voluptate in doloremque repellendus?
+                                    Provident cupiditate modi, iusto atque odio excepturi ea,
+                                    rerum a laudantium vitae nam eos?
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="bgimg">
+            <div class="caption">
+                <span class="border">
+                    <h2>Ayo Kunjungi Sekarang</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, beatae.</p>
+                </span>
+            </div>
+        </div>
+        <section>
+
+            <div class="galeri-kecil-wrap container-fluid">
+                <div class="container">
+                    <div class="row">
+                        <div class="text-galeri-k col-lg-5 col-md-5 text-white p-5">
+                            <h1>Nikmati Moment Anda Dengan Keluarga</h1>
+                        </div>
+                        <div class="col-lg-7 col-md-7 p-3">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-12 mb-4 mb-lg-0">
+                                    <div class="gallery">
+                                        <img class="w-100 shadow-1-strong rounded mb-4" src="assets/images/gallery/1.jpg"
+                                            alt="" />
+                                        <img class="w-100 shadow-1-strong rounded mb-4" src="assets/images/gallery/2.jpg"
+                                            alt="" />
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 mb-4 mb-lg-0">
+                                    <div class="gallery">
+                                        <img class="w-100 shadow-1-strong rounded mb-4" src="assets/images/gallery/3.jpg"
+                                            alt="" />
+                                        <img class="w-100 shadow-1-strong rounded mb-4" src="assets/images/gallery/4.jpg"
+                                            alt="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="langgan-wrap container-fluid text-center py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="langgan col-12">
+                        <div class="langgan-text col-12">
+                            <h1 class="fw-bold">Berlanggan dengan Kami</h1>
+                            <p class="mt-2 mb-4">
+                                Dapatkan info dan berita terupdate dari kami
+                            </p>
+                            <input type="text" placeholder="Masukkan Email..." class="mb-4" />
+                            <br />
+                            <button>KIRIM</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
---
+++ resources/views/client/partial/footer.blade.php
@@ -0,0 +1,46 @@
+<footer>
+    <div class="container row-footer">
+        <footer class="row row-cols-12 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+            <div class="col-md-9">
+                <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+                    <img class="imagelogobawah" src="assets/images/Asset 2.png" alt="">
+                </a>
+                <h1 class="fw-bold">Desa Buahan</h1>
+                <h5>Jl. Buahan, Buahan, Kec. Tabanan, Kabupaten Tabanan</h5>
+            </div>
+            <div class="col-4 col-md-3 text-start">
+                <h5>Section</h5>
+                    <ul class="nav flex-column ">
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Beranda</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Tentang Kami</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Gallery</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Kontak Kami</a></li>
+                    </ul><br>
+            </div>
+        </footer>
+    </div>
+        <div class="footer footer-wrap container-fluid text-white p-3 text-start">
+            <div class="container d-flex flex-wrap justify-content-between align-items-center py-2 my-2">
+                <p class="m-0">@copyright. <a href="https://www.buahandesaku.com/"> Buahan Desaku</a></p>
+                <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+                        <!-- link instagram -->
+                        <li class="ms-3"><a class="text-muted" href="#"></a>
+                            <img class="social-media" src="assets/images/instagram.svg" alt="">
+                        </li>
+                        <!-- link youtube -->
+                        <li class="ms-3"><a class="text-muted" href="#"></a>
+                            <img class="social-media" src="assets/images/youtube.svg" alt="">
+                        </li>
+                        <!-- link facebook -->
+                        <li class="ms-3"><a class="text-muted" href="#"></a>
+                            <img class="social-media" src="assets/images/facebook.svg" alt="">
+                        </li>
+                        <!-- link tiktok -->
+                        <li class="ms-3"><a class="text-muted" href="#"></a>
+                            <img class="social-media" src="assets/images/tiktok.svg" alt="">
+                        </li>
+                </ul>
+            </div>
+
+        </div>
+    </footer>
