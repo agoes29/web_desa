d9609bb842ddf13cda6414a695238c69cb46cbe0
fluctlightcard <fluctlightcard@gmail.com>
2022-10-22 21:26:18 +0800

    blog card

--- resources/views/client/page/blog.blade.php
+++ resources/views/client/page/blog.blade.php
@@ -120,7 +120,7 @@
         <!-- end Card blog -->
         
 
-        <!-- Start Coba Coba -->
+        <!-- START COBA COBA -->
         <div class="blog" id="blog">
             <div class="container">
                 <div class="section-header text-center wow zoomIn" data-wow-delay="0.1s">
@@ -131,7 +131,7 @@
                     <div class="col-lg-6">
                         <div class="blog-item wow fadeInUp" data-wow-delay="0.1s">
                             <div class="blog-img">
-                                <img src="img/blog-1.jpg" alt="Blog">
+                                <img src="{{asset('assets/images/gallery/1.jpg')}}" alt="Blog">
                             </div>
                             <div class="blog-text">
                                 <h2>Lorem ipsum dolor sit amet</h2>
@@ -144,14 +144,14 @@
                                 <p>
                                     Lorem ipsum dolor sit amet elit. Nullam commodo mattis mi. Nullam eu erat lectus. Proin viverra risus vitae luctus. Proin ut ante, vitae sapien. Fusce sem ac erat rhoncus, ornare mattis nisl massa et eros vitae pulvin
                                 </p>
-                                <a class="btn" href="">Read More <i class="fa fa-angle-right"></i></a>
+                                <a class="btn btn-readmore" href="">Read More <i class="fa fa-angle-right"></i></a>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-6">
                         <div class="blog-item wow fadeInUp" data-wow-delay="0.3s">
                             <div class="blog-img">
-                                <img src="img/blog-2.jpg" alt="Blog">
+                                <img src="{{asset('assets/images/gallery/1.jpg')}}" alt="Blog">
                             </div>
                             <div class="blog-text">
                                 <h2>Lorem ipsum dolor sit amet</h2>
@@ -171,7 +171,7 @@
                 </div>
             </div>
         </div>
-        <!-- end Coba COba -->
+        <!-- END COBA COBA -->
 
 </main>
 @endsection
