e18952348c6e1d3848f380051a667bb33ed91df4
satriyadi <satriyadi3001@gmail.com>
2022-10-23 15:25:06 +0800

    add card carousel in blog page

--- resources/views/client/page/blog.blade.php
+++ resources/views/client/page/blog.blade.php
@@ -7,46 +7,108 @@
                 <div class="container">
                     <h2 class="my-5 text-center">Carousel #4</h2>
 
-                    <div class="owl-carousel slide-one-item">
-                        <div class="d-md-flex testimony-29101 align-items-stretch">
-                            {{-- <img class="image" src="{{ Storage::url($first->image) }}" alt=""> --}}
-                            <div class="image" style="background-image: url({{ Storage::url($first->image) }});"></div>
-                            <div class="text">
-                                <blockquote>
-                                    <p>&ldquo;{!!$first->content !!}&rdquo;</p>
-
-                                    <div class="author">&mdash; Craig Stephen</div>
-                                </blockquote>
-                            </div>
-                        </div> <!-- .item -->
-                        @foreach ($hot as $hot)
-                            <div class="d-md-flex testimony-29101 align-items-stretch">
-                                {{-- <img class="image" src="{{ Storage::url($first->image) }}" alt=""> --}}
-                                <div class="image" style="background-image: url({{ Storage::url($hot->image) }});"></div>
-                                <div class="text">
-                                    <blockquote>
-                                        <p>&ldquo;{!!$hot->content !!}&rdquo;</p>
-
-                                        <div class="author">&mdash; Craig Stephen</div>
-                                    </blockquote>
-                                </div>
-                            </div> <!-- .item -->
-
-                            @endforeach
-                        </div>
-
-
-                </div>
+    <section id="hero" >
+        <!-- Carousel wrapper -->
+        <div id="carouselVideoExample"  data-bs-interval="10000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+            <!-- Indicators -->
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselVideoExample" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselVideoExample" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselVideoExample" data-bs-slide-to="2"
+                    aria-label="Slide 3"></button>
             </div>
-            <!-- Carousel wrapper -->
-        </section>
+
+            <!-- Inner -->
+                <div class="carousel-item active">
+                    <img class="img-fluid" src="{{ Storage::url($first->image) }}" alt="">
+                    <div class="carousel-container d-md-block align-bottom">
+                        <div class="container">
+                            <div class="card w-50 w-md-100 mx-auto px-2 py-3 mt-5 rad-10">
+                                <h4 class="animate__animated animate__fadeInDown wputih">{{ $first->title }}</h4>
+                                <a href="#about"
+                                    class="btn-get-started animate__animated animate__fadeInUp scrollto">More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                @foreach ($hot as $hot)
+                <div class="carousel-item">
+                    <img class="img-fluid" src="{{ Storage::url($hot->image) }}" alt="">
+                    <div class="carousel-container d-md-block">
+                        <div class="container">
+                            <div class="card w-50 w-md-100 mx-auto px-2 py-3 my-5 rad-10">
+                                <h4 class="animate__animated animate__fadeInDown wputih">{{ $hot->title }}</h4>
+                                <a href="#about"
+                                    class="btn-get-started animate__animated animate__fadeInUp scrollto">More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+             <!-- <div class="carousel-inner">
+                <div class="carousel-item active" style="background-image:url({{url('assets/images/img.jpg')}})">
+                    <div class="carousel-container d-md-block">
+                        <div class="container">
+                            <div class="card w-50 w-md-100 mx-auto px-2 py-3 my-5 rad-10">
+                                <h4 class="animate__animated animate__fadeInDown wputih">Judul Blog 1</h4>
+                                <a href="#about"
+                                    class="btn-get-started animate__animated animate__fadeInUp scrollto">More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="carousel-item" style="background-image:url({{url('assets/images/img.jpg')}})">
+                    <div class="carousel-container d-md-block">
+                        <div class="container">
+                            <div class="card w-50 w-md-100 mx-auto px-2 py-3 my-5 rad-10">
+                                <h4 class="animate__animated animate__fadeInDown wputih">Judul Blog 2</h4>
+                                <a href="#about"
+                                    class="btn-get-started animate__animated animate__fadeInUp scrollto">More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="carousel-item" style="background-image:url({{url('assets/images/img.jpg')}})">
+                    <div class="carousel-container d-md-block">
+                        <div class="container">
+                            <div class="card w-50 w-md-100 mx-auto px-2 py-3 my-5 rad-10">
+                                <h4 class="animate__animated animate__fadeInDown wputih">Judul Blog 3</h4>
+                                <a href="#about"
+                                    class="btn-get-started animate__animated animate__fadeInUp scrollto">More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> -->
+
+            <!-- Inner -->
+
+            <!-- Controls -->
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselVideoExample"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselVideoExample"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+        <!-- Carousel wrapper -->
+    </section>
 
         <!-- START COBA COBA -->
         <div class="blog" id="blog">
             <div class="container">
                 <div class="section-header text-center wow zoomIn" data-wow-delay="0.1s">
-                    <p>From Blog</p>
-                    <h2>Latest Articles</h2>
+                    <p>Blog Desa</p>
+                    <h2>Artikel Terbaru</h2>
                 </div>
                 <div class="row">
                     @foreach ($blog as $bl)
@@ -81,5 +143,5 @@
             </div>
         </div>
         <!-- END COBA COBA -->
-    </main>
+</main>
 @endsection
